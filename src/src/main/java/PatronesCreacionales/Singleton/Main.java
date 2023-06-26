b1d964f18b32e06d66014cfd9a27b1e19eefbb66
Ignacio Perez <naachoperez11@gmail.com>
2023-06-26 09:36:34 -0300

    patron singleton

---
+++ src/src/main/java/PatronesCreacionales/Singleton/Main.java
@@ -0,0 +1,7 @@
+package PatronesCreacionales.Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/src/main/java/PatronesCreacionales/Singleton/Singleton.java
@@ -0,0 +1,5 @@
+package PatronesCreacionales.Singleton;
+
+public class Singleton {
+
+}
